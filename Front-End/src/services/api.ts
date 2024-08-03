f9d73443e0da732e9b046d1426bf56a4185f5a39
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-08-03 11:53:22 -0300

    raw: fake api

--- Front-End/src/services/api.ts
+++ Front-End/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://project-full-stack-1.onrender.com",
+  baseURL: "https://full-stack-cars-fake-api.vercel.app/",
   timeout: 20000,
 });
